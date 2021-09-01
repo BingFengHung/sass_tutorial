f98461fe91364560a9463047fd0abece5cf913a7
BingFengHung <joe94008@gmail.com>
2021-09-01 23:57:28 +0000

    partial @import

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Learn sass from The .Net Ninja
 Define variables with '$' then variable names
 
 use '$' with variable name in css property value.
+
+## SASS Partial @import
+- sass partial 可以分割 scss 檔案，再將他們透過 `@import` 引入
+- 檔案前面加上 _ 代表這個檔案是 partial sass 編譯器不會去編譯此檔案
--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'))
 
 // comile sass into css
 function buildStyles() {
-	return src('index.scss')
+	return src('*.scss')
 	       .pipe(sass()) // convert scss to css
 				 .pipe(dest('css'))  // relative path
 }
 
 // watch source file change and run "buildStyles" automatically 
 function watchTask() {
-	watch(['index.scss'], buildStyles) 
+	watch(['*.scss'], buildStyles) 
 }
 
 // run function series
